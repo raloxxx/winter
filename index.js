1f004db7285e6cc6dc5b7a9815529748aa831068
Juan Manuel Delgado <juan.15121001@gmail.com>
2020-04-20 17:05:43 -0500

    cors

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+})
+
+
+
 // ------------------------------------------------------- //
 // Sets our app to use the handlebars engine
 app.set('views', path.join(__dirname, 'views'))
